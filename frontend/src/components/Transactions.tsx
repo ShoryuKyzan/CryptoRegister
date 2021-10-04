8e15e7714e9ec08f25db77bffa53b01eafb5f17d
Joseph Fleming <32823296+ShoryuKyzan@users.noreply.github.com>
2021-10-03 23:32:44 -0400

    responsive crypto transaction table.

--- frontend/src/components/Transactions.tsx
+++ frontend/src/components/Transactions.tsx
@@ -38,6 +38,24 @@ const transactions: Transaction[] = [
         'item': 'Rounded Diamond',
         'amount': 0.1,
         'crypto': cryptos[2]
+    },
+    {
+        'merchant': 'Haven',
+        'item': 'Flying Dog',
+        'amount': 2000000000002.11,
+        'crypto': cryptos[0]
+    },
+    {
+        'merchant': 'Moonbeam',
+        'item': 'Macaroni',
+        'amount': 0.2,
+        'crypto': cryptos[2]
+    },
+    {
+        'merchant': 'Trailblazer',
+        'item': 'Silver Star Pin',
+        'amount': 1,
+        'crypto': cryptos[1]
     }
 ]; // XXX dummy data
 
@@ -47,18 +65,31 @@ class Transactions extends React.Component {
         const rows: JSX.Element[] = [];
         this.data.forEach((t: Transaction) => {
             rows.push(
-            <div className="item">
+            <div className="row">
+                {/* mobile only */}
+                <div className="header">
+                    <span className="itemHeader">Merchant Name</span>
+                    <span className="itemHeader">Item</span>
+                </div>
                 <div className="merchant">{t.merchant}</div>
                 <div className="item">{t.item}</div>
-                <div className="amount">{t.amount}</div>
+                {/* mobile only */}
+                <div className="header secondHeader">
+                    <span className="itemHeader">Amount</span>
+                </div>
+                {/* extra currency indicator is mobile only */}
+                <div className="amount">{t.amount}<span className="currency">{t.crypto.name}</span></div>
                 <div className="currency">{t.crypto.name}</div>
-                <div className="currencyPrice">{t.crypto.price}</div>
+                {/* desktop only */}
+                <div className="currencyPriceDesktop">${t.crypto.price}</div>
+                {/* mobile only */}
+                <div className="conversion">x ${t.crypto.price} USD/{t.crypto.name} = </div>
                 <div className="total">${t.crypto.price * t.amount}</div>
             </div>);
         });
         return (
             <div className="Transactions">
-                <div className="item">
+                <div className="row desktopHeader">
                     <div className="merchant">Merchant Name</div>
                     <div className="item">Item</div>
                     <div className="amount">Amount (Crypto)</div>
