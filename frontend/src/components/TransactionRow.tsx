7a8497ed8ed0375280d09b81a4a2f087785a6694
Joseph Fleming <32823296+ShoryuKyzan@users.noreply.github.com>
2021-10-04 02:48:20 -0400

    toggleable editing controls

--- frontend/src/components/TransactionRow.tsx
+++ frontend/src/components/TransactionRow.tsx
@@ -2,10 +2,52 @@ import React from 'react';
 import {Transaction} from 'types/transaction'; // XXX dummy data
 import './TransactionRow.scss'
 
+interface _EFProps {
+    editing: boolean;
+    name: string;
+    value: string|number;
+    onChange: (name: string, value: string) => void;
+};
+function EditableTextField(props: _EFProps){
+    let n = <span>{props.value}</span>;
+    if(props.editing){
+        n = <input value={props.value} onChange={(e) => props.onChange(props.name, e.target.value)} />;
+    }
+    return (
+        n
+    );
+}
+
+interface _TransactionValues {
+    [name: string]: string|number;
+};
+
 interface _Props {
     transaction: Transaction
 };
-export class TransactionRow extends React.Component<_Props> {
+interface _State {
+    editing: boolean,
+    editingValues: _TransactionValues
+};
+export class TransactionRow extends React.Component<_Props, _State> {
+    constructor(props: _Props){
+        super(props)
+        this.state = {
+            editing: false,
+            editingValues: {
+                'merchant': props.transaction.merchant,
+                'item': props.transaction.item,
+                'amount': props.transaction.amount,
+                'cryptoName': props.transaction.crypto.name
+            }
+        }
+    }
+
+    setFieldValue(name: string, val: string){
+        const o: _TransactionValues = {};
+        o[name] = val;
+        this.setState({editingValues: o});
+    }
     render(){
         const t = this.props.transaction;
         return (
@@ -14,18 +56,52 @@ export class TransactionRow extends React.Component<_Props> {
                 <div className="header mobile">
                     <div className="cellHeader">Merchant Name</div>
                     <div className="cellHeader">Item</div>
-                    <div className="merchant">{t.merchant}</div>
-                    <div className="item">{t.item}</div>
+                    <div className="merchant">
+                        <EditableTextField name="merchant"
+                            onChange={(name, val) => this.setFieldValue(name, val)}
+                            editing={this.state.editing} value={t.merchant}/>
+                    </div>
+                    <div className="item">
+                        <EditableTextField name="item"
+                            onChange={(name, val) => this.setFieldValue(name, val)}
+                            editing={this.state.editing} value={t.item}/>
+                    </div>
                 </div>
-                <div className="merchant desktop">{t.merchant}</div>
-                <div className="item desktop">{t.item}</div>
+                <div className="merchant desktop">
+                    <EditableTextField name="merchant"
+                        onChange={(name, val) => this.setFieldValue(name, val)}
+                        editing={this.state.editing} value={t.merchant}/>
+                </div>
+                <div className="item desktop">
+                    <EditableTextField name="item"
+                        onChange={(name, val) => this.setFieldValue(name, val)}
+                        editing={this.state.editing} value={t.item}/>
+                </div>
                 <div className="header secondHeader mobile">
                     <span className="cellHeader">Amount</span>
                 </div>
-                <div className="amount">{t.amount}<span className="currency">{t.crypto.name}</span></div>
+                <div className="amount">
+                    <EditableTextField name="amount"
+                        onChange={(name, val) => this.setFieldValue(name, val)}
+                        editing={this.state.editing} value={t.amount}/>
+                    <span className="currency">
+                        <EditableTextField name="cryptoName"
+                            onChange={(name, val) => this.setFieldValue(name, val)}
+                            editing={this.state.editing} value={t.crypto.name}/>
+                    </span>
+                </div>
                 <div className="currencyPrice desktop">${t.crypto.price}</div>
                 <div className="conversion">x ${t.crypto.price} USD/{t.crypto.name} = </div>
                 <div className="total">${t.crypto.price * t.amount}</div>
+                <div className="actions">
+                    <button className={this.state.editing ? 'hide' : 'show'}
+                         onClick={() => this.setState({editing: true})}>Edit</button>
+                    <button className={this.state.editing ? 'show' : 'hide'}
+                        onClick={() => this.setState({editing: false})}>Done</button>
+                    <button className={this.state.editing ? 'hide' : 'show'}>
+                        Delete
+                    </button>
+                </div>
             </div>
         );
     }
