fa8dbdcb347c86f4a926a077fa2001b6aeeaac13
Joseph Fleming <32823296+ShoryuKyzan@users.noreply.github.com>
2021-10-04 04:11:37 -0400

    loading animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,54 @@
 import React from 'react';
 import Header from './components/Header';
 import Transactions from './components/Transactions';
+import ReactLoading from 'react-loading';
 import './App.scss'
 
-class App extends React.Component {
+interface _State {
+  loading: boolean
+};
+class App extends React.Component<{}, _State> {
   appName: string = "Crypto Register"
+  
+  constructor(props: {}){
+    super(props)
+    this.state = {
+      loading: false
+    };
+  }
+
+  componentDidMount(){
+    //this.setState({loading: true});
+  }
+
   render(){
+    let isLoading = <div></div>;
+    let loadingClass = '';
+    if(this.state.loading){
+      loadingClass = 'loading';
+      isLoading = <div className="loadingWrapper">
+        <div className="banner">
+        </div>
+        <div className="loadingWrapper2">
+          <div className="loader">
+              <ReactLoading
+              type={"bars"}
+              color={"green"}
+              height={100}
+              width={100}/>
+          </div>
+        </div>
+      </div>;
+    }
     return (
       <div className="App">
         <div className="header">
           <Header name={this.appName}/>
         </div>
-        <div className="content">
+        <div className={"content " + loadingClass}>
           {/* scrollable container */}
           <Transactions/>
+          {isLoading}
         </div>
       </div>
     );
